ba698b854c770bdb94cf33fb2df19ad7311379de
alexandra-code <57416878+alexandra-code@users.noreply.github.com>
2019-11-05 14:53:54 -0600

    Add files via upload

---
+++ chapter7/raincode_NotAsComplex.py
@@ -0,0 +1,110 @@
+#Rainfall list collector thing STAR Institute
+#Instructional Puposes only
+
+
+rain = []
+
+
+def computeRainAmt(rain):
+   
+    x = 0
+    while x != 12:
+        y = int(input("What is the total amount of rainfall for the target month? "))
+        rain.append(y)
+        x+=1
+
+    return(rain)
+
+
+def computeRainTotal(rain):
+
+    rainTotal = sum(rain)
+    print(rainTotal)
+
+
+def computeRainAvg(rain):
+
+    rainAvg = sum(rain) / len(rain)
+    print(rainAvg)
+    
+
+
+def rainHighest(rain):
+    highest = max(rain)
+    print(highest)
+
+def rainLowest(rain):
+    lowest = min(rain)
+    print(lowest)
+
+
+
+    
+
+
+computeRainAmt(rain)
+print(rain)
+computeRainTotal(rain)
+computeRainAvg(rain)
+rainHighest(rain)
+rainLowest(rain)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Made by Alexandra with the STAR institute.
